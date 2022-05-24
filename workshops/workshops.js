79472822636f529c47d970c66dd6bd1f64d3a2e0
sirdan <stanford303@gmail.com>
2022-05-24 12:10:12 -0700

    added getclass, creat participants, deleteparticipants

---
+++ workshops/workshops.js
@@ -0,0 +1,31 @@
+import { checkAuth, logout, getClass, deleteParticipants } from '../fetch-utils.js';
+import { renderCLass } from '../render-utils';
+
+checkAuth();
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
+async function displayClass() {
+    const main = document.querySelector('main');
+    main.textContent = '';
+    const data = await getClass();
+    for (let Class of data) {
+        const ClassEl = renderCLass(Class);
+        const ul = document.createElement('ul');
+        for (let participants of Class.participants) {
+            const li = document.createElement('li');
+            li.textContent = `${participants.name}: ${participants.contact}`;
+            li.addEventListener('click', async () => {
+                await deleteParticipants(participants.id);
+                await displayClass();
+            });
+            ul.append(li);
+        }
+        ClassEl.append(ul);
+        main.append(ClassEl);
+    }
+}
+displayClass();
